a14409cea0b734ba13e739a17cb960021176d51b
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-11 22:05:01 +0530

    redirecting to auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom'
 import Auth from "./pages/auth"
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/auth" element={<Auth />}/>
-
+      <Route path="*" element={<Navigate to="/auth"/>}/>
     </Routes>
     </BrowserRouter>
   );
