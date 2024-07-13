d81add9f1af96455cb279e3617f1d2fc287328dc
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-13 22:33:05 +0530

    login functional

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -27,7 +27,7 @@ const Auth = () => {
                             <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
                         </TabsList>
                         <TabsContent className="" value="login">
-                            <Input placeholder="Email" type="email" className="rounded-full p-6" value={email} onChange={e=>setEmail(e.target.value)} />
+                            <Input placeholder="Email" type="email" className="rounded-full p-6" value={email} onChange={(e)=>setEmail(e.target.value)} />
                         </TabsContent>
                         <TabsContent className="" value="signup"></TabsContent>
                     </Tabs>
