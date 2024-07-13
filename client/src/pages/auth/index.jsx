7192c1a42464f48f7aee0823065761be7b776756
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-13 22:04:07 +0530

    States added

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,7 +1,11 @@
 import Background from "@/assets/login2.png"
 import Victory from "@/assets/victory.svg"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs"
+import { Tabs, TabsList } from "@/components/ui/tabs"
+import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs"
+import { useState } from "react"
 const Auth = () => {
+    const [email, setEmail] = useState("")
+
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
         <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
