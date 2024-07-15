7c32c98d36751c0c7e106b1fcd2bbb5ad7e1d2a8
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-15 21:34:26 +0530

    Authorisation controller

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,16 @@
+import User from "./models/UserModel";
+
+export const signup = async (request,response,next) => {
+    try{
+        const { email,password } = req.body;
+        if(!email || !password) {
+            return response.status(400).send("Email and Password is required.")
+        }
+        const user = await User.create({emal,password});
+
+    }
+    catch(error){
+        console.log({error});
+        return response.status(500).send("Internal Server Error.");
+    }
+}
