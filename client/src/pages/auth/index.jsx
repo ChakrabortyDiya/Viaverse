7bd3dba2e7045fff65ed20cca6902103d1e2b60d
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-17 22:16:20 +0530

    minor commit

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -35,7 +35,7 @@ const Auth = () => {
     }
     const handleSignup = async ()=> {
         if(validateSignup()) {
-            const response = await apiClient.post(SIGNUP_ROUTE,(email,password));
+            const response = await apiClient.post(SIGNUP_ROUTE,{email,password});
             console.log({ response });
         }
     }
