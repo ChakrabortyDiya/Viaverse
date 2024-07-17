13d096340734ba91548bb01ec6a53a06a862a492
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-17 21:57:11 +0530

    minor commit

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken"
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (email,userId) =>{
-    return jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn: maxAge});
+    return jwt.sign({email,userId}, process.env.JWT_KEY,{expiresIn: maxAge});
 };
 
 export const signup = async (request,response,next) => {
@@ -13,7 +13,7 @@ export const signup = async (request,response,next) => {
         if(!email || !password) {
             return response.status(400).send("Email and Password is required.")
         }
-        const user = await User.create({emal,password});
+        const user = await User.create({email,password});
         response.cookie("jwt",createToken(email,user.id),{
             maxAge,secure: true,
             sameSite: "None",
