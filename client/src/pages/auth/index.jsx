34bb9cbc26cbca3e6fbe0ee6ce41ac7ecc485b11
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-21 21:27:09 +0530

    login routes added

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -35,7 +35,9 @@ const Auth = () => {
     }
     const handleSignup = async ()=> {
         if(validateSignup()) {
-            const response = await apiClient.post(SIGNUP_ROUTE,{email,password});
+            const response = await apiClient.post(SIGNUP_ROUTE,{email,password},
+                {withCredentials: true}
+            );
             console.log({ response });
         }
     }
